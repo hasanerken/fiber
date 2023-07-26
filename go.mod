2295bafa9fd4900f1e3c76857acf8a86da7d617c
Hasan Erken <hasan.erken@gmail.com>
2023-07-27 01:10:05 +0300

    database connected

--- go.mod
+++ go.mod
@@ -4,4 +4,12 @@ module fiber
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.42.0
+require (
+	github.com/friendsofgo/errors v0.9.2
+	github.com/gofiber/fiber/v2 v2.42.0
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/volatiletech/null/v8 v8.1.2
+	github.com/volatiletech/sqlboiler/v4 v4.14.2
+	github.com/volatiletech/strmangle v0.0.4
+)
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"os"
-
+	"context"
+	"fiber/infrastructure/storages"
+	"fiber/repositories/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
 )
 
 func getPort() string {
@@ -22,10 +27,39 @@ func main() {
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
-			"message":  "Hello, Railway!",
-			"database": os.Getenv("DATABASE_URL"),
+			"message": "Hello, Railway!",
 		})
 	})
 
-	app.Listen(getPort())
+	client, err := storages.NewPostgreSQLConnection()
+	if err != nil {
+		// Log a fatal error if the connection cannot be established
+		log.Fatalf("failed to create sql client: %s", err)
+	}
+
+	// Defer the closing of the database connection to ensure that it is always closed
+	defer func(client *sqlx.DB) {
+		err := client.Close()
+		if err != nil {
+			// Log a fatal error if the connection cannot be closed
+			log.Fatalf("failed to close postgres client: %s", err)
+		}
+	}(client)
+
+	err = client.Ping()
+	if err != nil {
+		log.Fatalf("No ping from db")
+	}
+
+	app.Get("/tenants", func(c *fiber.Ctx) error {
+		all, err := models.Tenants().All(context.Background(), client)
+		if err != nil {
+			return err
+		}
+		return c.JSON(fiber.Map{
+			"tenants": all,
+		})
+	})
+
+	log.Fatal(app.Listen(getPort()))
 }
