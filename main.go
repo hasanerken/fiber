908ef37748aaa247ad65ae7a8249b3a02dae7dd5
Hasan Erken <hasan.erken@gmail.com>
2023-07-27 19:39:38 +0300

    cors added

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fiber/infrastructure/storages"
 	"fiber/repositories/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
@@ -18,12 +19,12 @@ func getPort() string {
 	} else {
 		port = ":" + port
 	}
-
 	return port
 }
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
